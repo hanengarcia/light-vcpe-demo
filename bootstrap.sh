fba32986fd7de621af2f69bb3629687646cd34e1
Hanen Garcia <hgarciag@redhat.com>
2017-10-26 11:54:30 -0400

    Adding SSH tunnel to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ then
 	echo "ssh key $ssh_key_private found, skipping"
 	cat $ssh_key_public
 else
-	echo "ssh key $ssh_key_private not found, generating new one"
+	echo "ssh key $ssh_key_private not found, generating new"
 	ssh-keygen -t rsa -b 2048 -f $ssh_key_private -N ""
 fi
 
@@ -18,7 +18,6 @@ if [ -e $ssh_authorized_keys]
 then
 	echo "ssh authorized keys $ssh_authorized_keys found, appending key"
 	cat $ssh_key_public >> $ssh_authorized_keys
-	
 else
 	echo "ssh authorized keys $ssh_authorized_keys not found, generating new one"
 	cp $ssh_key_public $ssh_authorized_keys
@@ -30,7 +29,40 @@ proxy_user=`hostname -s`
 proxy_host='198.154.188.151'
 proxy_port='8080'
 
+### Call Ansible Tower
+curl --silent --insecure -X POST \
+  https://tower.keepontouch.net:443/api/v2/job_templates/9/launch/ \
+  -H 'authorization: Basic YWRtaW46cmVkaGF0OTk=' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'postman-token: 7aafe9c1-f3fa-60cb-8787-6567f0d19d3c' \
+  -d "{
+  \"extra_vars\": 
+  {
+    \"remote_hostname\": \"${proxy_user}\",
+    \"remote_key_public\": \"${ssh_key_public}\"
+   }
+}"
+
+
 ### Establish SSH tunnel
-ssh –f –N –T –R 0.0.0.0:${proxy_port}:localhost:22 ${proxy_user}@${proxy_host}
+echo "\n\n\nssh –f –N –T –R 172.28.1.20:${proxy_port}:localhost:22 ${proxy_user}@${proxy_host}\n\n\n"
+
+echo -n "Establishing tunnel ."
+
+connected=false
+while [ $connected = false ]
+do
+	# Create the SSH tunnel 
+	ssh -f -N -T -R 172.28.1.20:${proxy_port}:localhost:22 ${proxy_user}@${proxy_host}
+	
+	if [ $? -eq 0 ]; then
+		connected=true
+		echo " done!"
+	else
+		sleep 10
+		echo -n "."
+	fi
+done
 
 echo "This device is now managed via ${proxy_host}:${proxy_port}"
